efebff0ecf43dafd0af36db0b843698b959cbe2f
Darren Rambaud <dtrambaud@txstate.edu>
2017-02-09 14:57:32 -0600

    Added author line

--- fractal_omp.cpp
+++ fractal_omp.cpp
@@ -21,6 +21,7 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 
 Author: Martin Burtscher
+Co-Author: Darren Rambaud
 */
 
 #include <cstdlib>
